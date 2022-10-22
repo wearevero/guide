2431eb2a51b3f3247a790d71775efb62afefdaba
yuxxeun <itsyuxxeun@gmail.com>
2022-10-22 20:11:05 +0700

    init

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+module.exports = eleventyConfig => {
+  const markdownItOpts = {
+    html: true,
+    breaks: true,
+    linkify: true
+  }
+
+  const markdownIt = require('markdown-it')
+  const markdownItCheckbox = require('markdown-it-checkbox')
+
+  const markdownLib = markdownIt(markdownItOpts).use(markdownItCheckbox)
+
+  
+  eleventyConfig.addPassthroughCopy('css')
+  eleventyConfig.setLibrary('md', markdownLib)
+
+  return {
+    pathPrefix: "/",
+    passthroughFileCopy: true
+  }
+}
---
+++ READE.md
@@ -0,0 +1,7 @@
+---
+permalink: 'index.html'
+title: 'Resources by valinatic R&D'
+excerpt: 'https://valinatic.github.io/resources'
+---
+
+# Learning Resources
